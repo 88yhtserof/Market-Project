1e108251195591f3a78a7fc951390095c62e9c2d
88yhtserof <88yhtserofg@gmail.com>
2025-02-26 02:14:23 +0900

    [DEL] 불필요한 이전 코드 삭제 #1

--- Market-Project/Market-Project/Controllers/SearchResultViewController.swift
+++ Market-Project/Market-Project/Controllers/SearchResultViewController.swift
@@ -78,50 +78,6 @@ final class SearchResultViewController: BaseViewController {
             .disposed(by: disposeBag)
     }
     
-//    private func bindWhenInit() {
-        /*
-         bind 메서드의 호출 시점:
-         bind 메서드 호출보다 네트워크 통신 호출이 빠르기 때문에 searchWord는 빈값으로 전달되고 네트워크 통신 오류가 발생한다. 따라서 bind 메서드는 SearchResultVC의 초기화 시점에 호출되어야 한다.
-         */
-//        viewModel.outputSearchWord.lazyBind { [weak self] searchWord in
-//            guard let self else { return }
-//            print("outputSearchWord bind")
-//            self.searchWord = searchWord
-//            self.navigationItem.title = searchWord
-//        }
-//    }
-    
-//    private func bindWhenViewDidLoad() {
-//        
-//        viewModel.outputShowErrorAlert.lazyBind { [weak self] errorMessage in
-//            guard let self else { return }
-//            print("outputShowErrorAlert bind")
-//            self.showAlert(title: "검색 오류", message: errorMessage, Style: .alert) {[weak self] _ in
-//                self?.navigationController?.popViewController(animated: true)
-//            }
-//        }
-//        
-//        viewModel.outputTotalItemsCountString.lazyBind { [weak self] total in
-//            guard let self else { return }
-//            print("outputTotalItemsCountString bind")
-//            self.mainView.totalLabel.argument = total
-//        }
-//        
-//        viewModel.outputReloadData.lazyBind { [weak self] _ in
-//            guard let self else { return }
-//            print("outputReloadData bind")
-//            self.mainView.collectionView.reloadData()
-//        }
-//        
-//        viewModel.outputScrollToTop.lazyBind { [weak self] _ in
-//            guard let self else { return }
-//            print("outputScrollToTop bind")
-//            self.mainView.collectionView.contentOffset = .zero
-//        }
-//        
-//        viewModel.inputLoadSearchResult.send()
-//    }
-    
     //MARK: - Actions
     @objc func sortButtonDidTapped(_ sender: SelectButton) {
         sender.isShownSelected = true
@@ -132,24 +88,3 @@ final class SearchResultViewController: BaseViewController {
         sortButtonDidTapped.accept(sender.tag)
     }
 }
-
-//MARK: - CollectionView Delegate
-//extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return viewModel.outputMarketItems.value.count
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.identifier, for: indexPath) as? SearchResultCollectionViewCell else {
-//            print("Could not find cell")
-//            return UICollectionViewCell()
-//        }
-//        let item = viewModel.outputMarketItems.value[indexPath.item]
-//        cell.configure(with: item)
-//        return cell
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-//        viewModel.inputWillDisplayItems.send(indexPath)
-//    }
-//}
--- Market-Project/Market-Project/Controllers/SearchResultViewModel.swift
+++ Market-Project/Market-Project/Controllers/SearchResultViewModel.swift
@@ -105,108 +105,14 @@ final class SearchResultViewModel: BaseViewModel {
                 searchText.accept(owner.searchText)
             }
             .disposed(by: disposeBag)
-        
-//        guard let sort = MarketItemSort(rawValue: sortNumber) else {
-//            print("Unknown sort")
-//            return
-//        }
-//        guard self.sort != sort else {
-//            print("already selected")
-//            return
-//        }
-//        self.sort = sort
-//        self.outputScrollToTop.send()
-//        self.loadMarketItems(sort: sort, isInitialLoad: true)
-        
+
         return Output(searchText: searchText,
                       searchResultItems: searchResultItems,
                       totalSearchResultCount: totalSearchResultCount,
                       errorMessage: errorMessage)
     }
     
-//    // IN
-//    let inputLoadSearchResult: Observable<Void> = Observable(())
-//    let inputWillDisplayItems: Observable<IndexPath?> = Observable(nil)
-//    let inputSelectSort: Observable<Int> = Observable(0)
-//    
-//    // OUT
-//    let outputSearchWord: Observable<String?> = Observable(nil)
-//    let outputMarketItems: Observable<[MarketItem]> = Observable([])
-//    let outputShowErrorAlert: Observable<String> = Observable("")
-//    let outputTotalItemsCountString: Observable<String> = Observable("")
-//    let outputReloadData: Observable<Void> = Observable(())
-//    let outputScrollToTop: Observable<Void> = Observable(())
-    
-//    // DAT
-    
-//    init() {
-//        print("SearchResultViewModel init")
-        
-//        inputLoadSearchResult.lazyBind { [weak self] _ in
-//            guard let self else { return }
-//            print("inputLoadSearchResult bind")
-//            self.loadMarketItems(sort: self.sort, isInitialLoad: true)
-//        }
-//        
-//        inputWillDisplayItems.lazyBind { [weak self] indexPath in
-//            guard let self else { return }
-//            print("inputWillDisplayItems bind")
-//            guard let indexPath else { return }
-//            self.willDisplay(at: indexPath)
-//        }
-//        
-//        inputSelectSort.lazyBind { [weak self] sortNumber in
-//            guard let self else { return }
-//            print("inputSelectSort bind")
-//            self.didSelectSort(sortNumber)
-//        }
-//    }
-    
     deinit {
         print("SearchResultViewModel deinit")
     }
-    
-//    private func loadMarketItems(sort: MarketItemSort, isInitialLoad: Bool) {
-//        guard let searchWord = outputSearchWord.value else { return }
-//        NetworkManager.shared.getShopList(searchWord: searchWord, sort: sort, start: String(start)) {(value, error) in
-//            if let error {
-//                self.outputShowErrorAlert.send(error)
-//                return
-//            }
-//            
-//            guard let value else { return }
-//            if isInitialLoad {
-//                self.start = 0
-//                self.outputMarketItems.send([])
-//                self.outputTotalItemsCountString.send(value.total.decimal() ?? "")
-//            }
-//            self.outputMarketItems.value.append(contentsOf: value.items)
-//            self.start += NetworkManager.Pagenation.market.display
-//            self.total = value.total
-//            self.outputReloadData.send(())
-//        }
-//    }
-//    
-//    private func willDisplay(at indexPath: IndexPath) {
-//        let items = outputMarketItems.value
-//        guard items.count != self.total else { return }
-//        
-//        if (items.count - 2) == indexPath.item {
-//            loadMarketItems(sort: sort, isInitialLoad: false)
-//        }
-//    }
-//    
-//    private func didSelectSort(_ sortNumber: Int) {
-//        guard let sort = MarketItemSort(rawValue: sortNumber) else {
-//            print("Unknown sort")
-//            return
-//        }
-//        guard self.sort != sort else {
-//            print("already selected")
-//            return
-//        }
-//        self.sort = sort
-//        self.outputScrollToTop.send()
-//        self.loadMarketItems(sort: sort, isInitialLoad: true)
-//    }
 }
